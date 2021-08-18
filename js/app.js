21d60881675e69688434fdb9aa4196f343522f7b
Nicole Schlagbauer <Nicole.Schlagbauer@proleit.com>
2021-08-18 14:01:05 +0200

    Implemented highlighting of active section

--- js/app.js
+++ js/app.js
@@ -19,14 +19,29 @@
 */
 const sections = document.getElementsByTagName('section');
 const navList = document.getElementById('navbar__list');
+const navBar = document.querySelector('.navbar__menu');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function isScrolledIntoView(element) {
+  const rect = element.getBoundingClientRect();
+  const elementTop = rect.top;
+  const elementBottom = rect.bottom;
+  const navBarHeight = navBar.clientHeight;
+  const contentHeight = window.innerHeight - navBarHeight;
 
 
+  console.log('contentHeight: ' + contentHeight);
+
+// element is active while its top is sitting above 50% of window height and it's bottom is below the 50% window height
+  const isVisible = elementTop < contentHeight-(contentHeight/2)+navBarHeight && elementBottom >= contentHeight/2+navBarHeight;
+  return isVisible;
+}
+
+document.addEventListener('scroll', checkVisibleSection);
 
 /**
  * End Helper Functions
@@ -45,12 +60,19 @@ for (const section of sections) {
 
 navList.appendChild(docFrag);
 
-const endTime = performance.now();
-const timeSpan = endTime - startTime;
-console.log('This took ' + timeSpan + 'ms');
-
 // Add class 'active' to section when near top of viewport
+function checkVisibleSection () {
+  for (const section of sections) {
+    if (isScrolledIntoView(section)) {
+      section.classList.add('active-section');
+      console.log('Element ' + section.id + ' is active');
+    }
+    else {
+      section.classList.remove('active-section');
+    }  
+}
 
+}
 
 // Scroll to anchor ID using scrollTO event
 
